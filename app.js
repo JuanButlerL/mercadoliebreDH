9b7857dd5fff60a958a6d8bdb42bc267fc3dcfab
Butler <JBUTLERL@ford.com>
2022-08-28 17:12:43 -0300

    ajustes finales

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,"./public")));
 
-app.get('/', (req, res) => {
-    res.sendFile (path.join(__dirname,"/views/home.html"))   
-})
 
 app.listen(3001,() => {
     console.log('Servidor corriendo')
 });
+
+app.get('/', (req, res) => {
+    res.sendFile (path.join(__dirname,"/views/home.html"))   
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile (path.join(__dirname,"/views/register.html"))   
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile (path.join(__dirname,"/views/login.html"))   
+})
